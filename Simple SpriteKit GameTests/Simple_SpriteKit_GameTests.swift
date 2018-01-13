81a36b7f3dd380c668614fa85b7bfb54e7a1dfb2
Daniel Pratt <daniel@blaumagier.com>
2018-01-12 19:22:58 -0800

    Initial Commit

---
+++ Simple SpriteKit GameTests/Simple_SpriteKit_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Simple_SpriteKit_GameTests.swift
+//  Simple SpriteKit GameTests
+//
+//  Created by Daniel Pratt on 1/12/18.
+//  Copyright © 2018 Daniel Pratt. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_SpriteKit_Game
+
+class Simple_SpriteKit_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
