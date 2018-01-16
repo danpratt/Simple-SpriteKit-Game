00a78312368c020e93a1be6256ef2ca195c8c69a
Daniel Pratt <daniel@blaumagier.com>
2018-01-15 18:11:16 -0800

    feat: added physics and sound

--- Simple SpriteKit Game/GameScene.swift
+++ Simple SpriteKit Game/GameScene.swift
@@ -45,7 +45,16 @@ extension CGPoint {
     
 }
 
-class GameScene: SKScene {
+// Physics Category
+struct PhysicsCategory {
+    static let None : UInt32 = 0
+    static let All : UInt32 = UInt32.max
+    static let Monster: UInt32 = 0b1  // 1
+    static let ThrowingStar: UInt32 = 0b10 // 2
+    
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     let player = SKSpriteNode(imageNamed: "player")
     
     override func didMove(to view: SKView) {
@@ -54,12 +63,19 @@ class GameScene: SKScene {
         
         addChild(player)
         
+        physicsWorld.gravity = CGVector.zero
+        physicsWorld.contactDelegate = self
+        
         run(SKAction.repeatForever(
             SKAction.sequence([
                 SKAction.run(addMonster),
                 SKAction.wait(forDuration: 1.0)
                 ])
         ))
+        
+        let backgroundMusic = SKAudioNode(fileNamed: "background-music-aac.caf")
+        backgroundMusic.autoplayLooped = true
+        addChild(backgroundMusic)
     }
     
     func random() -> CGFloat {
@@ -73,6 +89,11 @@ class GameScene: SKScene {
     func addMonster() {
         // create the sprite
         let monster = SKSpriteNode(imageNamed: "monster")
+        monster.physicsBody = SKPhysicsBody(rectangleOf: monster.size)
+        monster.physicsBody?.isDynamic = true
+        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster
+        monster.physicsBody?.contactTestBitMask = PhysicsCategory.ThrowingStar
+        monster.physicsBody?.collisionBitMask = PhysicsCategory.None
         
         // determine where to spawn the monster along the y axis
         let actualY = random(min: monster.size.height / 2, max: size.height - monster.size.height)
@@ -103,11 +124,21 @@ class GameScene: SKScene {
         let throwingStar = SKSpriteNode(imageNamed: "projectile")
         throwingStar.position = player.position
         
+        // Setup Physics
+        throwingStar.physicsBody = SKPhysicsBody(circleOfRadius: throwingStar.size.width/2)
+        throwingStar.physicsBody?.isDynamic = true
+        throwingStar.physicsBody?.categoryBitMask = PhysicsCategory.ThrowingStar
+        throwingStar.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
+        throwingStar.physicsBody?.collisionBitMask = PhysicsCategory.None
+        throwingStar.physicsBody?.usesPreciseCollisionDetection = true
+        
         // setup offset
         let offset = touchlocation - throwingStar.position
         
         // don't throw backwards
         if offset.x < 0 { return }
+        
+        run(SKAction.playSoundFileNamed("pew-pew-lei.caf", waitForCompletion: false))
         
         // add the throwing star to the scene
         addChild(throwingStar)
@@ -125,7 +156,34 @@ class GameScene: SKScene {
         let actionMove = SKAction.move(to: realDest, duration: 2.0)
         let actionDone = SKAction.removeFromParent()
         throwingStar.run(SKAction.sequence([actionMove, actionDone]))
+    }
+    
+    // handle physics engine contact
+    func didBegin(_ contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
         
+        if ((firstBody.categoryBitMask & PhysicsCategory.Monster != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.ThrowingStar != 0)) {
+            if let monster = firstBody.node as? SKSpriteNode, let
+                throwingStar = secondBody.node as? SKSpriteNode {
+                throwingStarDidCollideWithMonster(throwingStar: throwingStar, monster: monster)
+            }
+        }
+    }
+    
+    // Handle removing monster after hit
+    func throwingStarDidCollideWithMonster(throwingStar: SKSpriteNode, monster: SKSpriteNode) {
+        print("Hit")
+        throwingStar.removeFromParent()
+        monster.removeFromParent()
     }
     
 }
